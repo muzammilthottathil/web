8c11c2a495c13560c60eec04edcdc48f5c83baf1
taqnar <muzammilthottathil.official@gmail.com>
2020-10-01 17:06:06 +0530

    counter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Header from "./Header";
+import "./style/style.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const resetFunction = () => {
+    console.log("reset triggered");
+  };
+
+  const increment = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+  const decrement = () => {
+    // do not go to -ve , stop at zero
+    console.log("decrement Triggered");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className="mid-section">
+        <div className="minus-button">
+          <span>-</span>
+        </div>
+
+        <div className="display-section">
+          <span>{count}</span>
+        </div>
+
+        <div className="plus-button">
+          <span onClick={increment}>+</span>
+        </div>
+      </div>
+
+      <div className="reset-button">
+        <span onClick={resetFunction}>Reset</span>
+      </div>
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style/style.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>COUNTER</h1>
+      <p>USING NEOMORPHISM DESIGN AND REACT JS FRAMEWORK</p>
+    </div>
+  );
+}
+
+export default Header;
